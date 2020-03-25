bd811542ab43025e02aa3219a9da985143674d57
Bartosz Lenar <bartosz@lenar.dev>
2020-03-25 17:54:38 +0100

    Added unit tests for exceptions

---
+++ tests/Validot.Tests.Unit/ValidotExceptionTests.cs
@@ -0,0 +1,30 @@
+namespace Validot.Tests.Unit
+{
+    using System;
+
+    using FluentAssertions;
+
+    using Xunit;
+
+    public class ValidotExceptionTests
+    {
+        [Fact]
+        public void Should_Initialize_With_Message()
+        {
+            var exception = new ValidotException("a message");
+
+            exception.Message.Should().Be("a message");
+        }
+
+        [Fact]
+        public void Should_Initialize_With_Message_And_InnerException()
+        {
+            var innerException = new Exception();
+
+            var exception = new ValidotException("a message", innerException);
+
+            exception.Message.Should().Be("a message");
+            exception.InnerException.Should().BeSameAs(innerException);
+        }
+    }
+}

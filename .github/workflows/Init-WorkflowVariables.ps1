53b2a76219c16aa1679fba65efff139f9ffb0d44
Bartosz Lenar <bartosz@lenar.dev>
2020-05-13 20:41:45 +0200

    Made ContinuousIntegrationBuild dependent on CI flag
    ContinuousIntegrationBuild is now true only then VALIDOT_CI environmental variable is also true.
    VALIDOT_CI is set in powershell script triggered by the CI system.

--- .github/workflows/Init-WorkflowVariables.ps1
+++ .github/workflows/Init-WorkflowVariables.ps1
@@ -4,11 +4,11 @@ if ($env:GITHUB_EVENT_NAME.Equals("release")) {
 
     $tag = $env:GITHUB_REF.Substring("refs/tags/".Length).TrimStart('v');
 
-    if ($tag -match "^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$") 
+    if ($tag -match "^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$")
     {
         $version = $tag
     }
-    else 
+    else
     {
         Write-Error "Tag contains invalid semver: $tag" -ErrorAction Stop
     }
@@ -19,3 +19,4 @@ else {
 
 Write-Output "::set-env name=VALIDOT_VERSION::$version"
 Write-Output "::set-env name=VALIDOT_COMMIT::$commitShortSha"
+Write-Output "::set-env name=VALIDOT_CI::true"

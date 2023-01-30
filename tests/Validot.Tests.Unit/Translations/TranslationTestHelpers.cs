6a84d31ac22c0b2c92ef11e6ce635e03f58b18ab
Bartosz Lenar <bartosz@lenar.dev>
2023-01-30 20:00:36 +0100

    Added helpers for testing placeholders in translations
    WIP helper

--- tests/Validot.Tests.Unit/Translations/TranslationTestHelpers.cs
+++ tests/Validot.Tests.Unit/Translations/TranslationTestHelpers.cs
@@ -1,8 +1,13 @@
 namespace Validot.Tests.Unit.Translations
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     using FluentAssertions;
+
+    using Validot.Errors.Args;
+    using Validot.Translations;
 
     public static class TranslationTestHelpers
     {
@@ -44,6 +49,99 @@ namespace Validot.Tests.Unit.Translations
                 selectedTranslation.Keys.Should().Contain(pair.Key);
 
                 selectedTranslation[pair.Key].Should().Be(pair.Value);
+            }
+        }
+
+        public static void ShouldContainOnlyValidPlaceholders(this IReadOnlyDictionary<string, string> translation)
+        {
+            TestPlaceholders(translation, MessageKey.Global.Error);
+            TestPlaceholders(translation, MessageKey.Global.Required);
+            TestPlaceholders(translation, MessageKey.Global.Forbidden);
+            TestPlaceholders(translation, MessageKey.Global.ReferenceLoop);
+            TestPlaceholders(translation, MessageKey.CharType.EqualToIgnoreCase, "value");
+            TestPlaceholders(translation, MessageKey.CharType.NotEqualToIgnoreCase, "value");
+            TestPlaceholders(translation, MessageKey.GuidType.EqualTo, "value");
+            TestPlaceholders(translation, MessageKey.GuidType.NotEqualTo, "value");
+            TestPlaceholders(translation, MessageKey.GuidType.NotEmpty, "value");
+            TestPlaceholders(translation, MessageKey.Collections.EmptyCollection);
+            TestPlaceholders(translation, MessageKey.Collections.NotEmptyCollection);
+            TestPlaceholders(translation, MessageKey.Collections.ExactCollectionSize, "size");
+            TestPlaceholders(translation, MessageKey.Collections.MaxCollectionSize, "max");
+            TestPlaceholders(translation, MessageKey.Collections.MinCollectionSize, "min");
+            TestPlaceholders(translation, MessageKey.Collections.CollectionSizeBetween, "min", "max");
+            TestPlaceholders(translation, MessageKey.Numbers.EqualTo, "value");
+            TestPlaceholders(translation, MessageKey.Numbers.NotEqualTo, "value");
+            TestPlaceholders(translation, MessageKey.Numbers.GreaterThan, "min");
+            TestPlaceholders(translation, MessageKey.Numbers.GreaterThanOrEqualTo, "min");
+            TestPlaceholders(translation, MessageKey.Numbers.LessThan, "max");
+            TestPlaceholders(translation, MessageKey.Numbers.LessThanOrEqualTo, "max");
+            TestPlaceholders(translation, MessageKey.Numbers.Between, "min", "max");
+            TestPlaceholders(translation, MessageKey.Numbers.BetweenOrEqualTo, "min", "max");
+            TestPlaceholders(translation, MessageKey.Numbers.NonZero);
+            TestPlaceholders(translation, MessageKey.Numbers.Positive);
+            TestPlaceholders(translation, MessageKey.Numbers.NonPositive);
+            TestPlaceholders(translation, MessageKey.Numbers.Negative);
+            TestPlaceholders(translation, MessageKey.Numbers.NonNegative);
+            TestPlaceholders(translation, MessageKey.Numbers.NonNaN);
+            TestPlaceholders(translation, MessageKey.Texts.Email, "value", "stringComparison");
+            TestPlaceholders(translation, MessageKey.Texts.EqualTo, "value", "stringComparison");
+            TestPlaceholders(translation, MessageKey.Texts.NotEqualTo, "value", "stringComparison");
+            TestPlaceholders(translation, MessageKey.Texts.Contains, "value", "stringComparison");
+            TestPlaceholders(translation, MessageKey.Texts.NotContains, "value", "stringComparison");
+            TestPlaceholders(translation, MessageKey.Texts.NotEmpty);
+            TestPlaceholders(translation, MessageKey.Texts.NotWhiteSpace);
+            TestPlaceholders(translation, MessageKey.Texts.SingleLine);
+            TestPlaceholders(translation, MessageKey.Texts.ExactLength, "length");
+            TestPlaceholders(translation, MessageKey.Texts.MaxLength, "max");
+            TestPlaceholders(translation, MessageKey.Texts.MinLength, "min");
+            TestPlaceholders(translation, MessageKey.Texts.LengthBetween, "min", "max");
+            TestPlaceholders(translation, MessageKey.Texts.Matches, "pattern");
+            TestPlaceholders(translation, MessageKey.Texts.StartsWith, "value", "stringComparison");
+            TestPlaceholders(translation, MessageKey.Texts.EndsWith, "value", "stringComparison");
+            TestPlaceholders(translation, MessageKey.Times.EqualTo, "value", "timeComparison");
+            TestPlaceholders(translation, MessageKey.Times.NotEqualTo, "value", "timeComparison");
+            TestPlaceholders(translation, MessageKey.Times.After, "min", "timeComparison");
+            TestPlaceholders(translation, MessageKey.Times.AfterOrEqualTo, "min", "timeComparison");
+            TestPlaceholders(translation, MessageKey.Times.Before, "max", "timeComparison");
+            TestPlaceholders(translation, MessageKey.Times.BeforeOrEqualTo, "max", "timeComparison");
+            TestPlaceholders(translation, MessageKey.Times.Between, "min", "max", "timeComparison");
+            TestPlaceholders(translation, MessageKey.Times.BetweenOrEqualTo, "min", "max", "timeComparison");
+            TestPlaceholders(translation, MessageKey.TimeSpanType.EqualTo, "value");
+            TestPlaceholders(translation, MessageKey.TimeSpanType.NotEqualTo, "value");
+            TestPlaceholders(translation, MessageKey.TimeSpanType.GreaterThan, "min");
+            TestPlaceholders(translation, MessageKey.TimeSpanType.GreaterThanOrEqualTo, "min");
+            TestPlaceholders(translation, MessageKey.TimeSpanType.LessThan, "max");
+            TestPlaceholders(translation, MessageKey.TimeSpanType.LessThanOrEqualTo, "max");
+            TestPlaceholders(translation, MessageKey.TimeSpanType.Between, "min", "max");
+            TestPlaceholders(translation, MessageKey.TimeSpanType.BetweenOrEqualTo, "min", "max");
+            TestPlaceholders(translation, MessageKey.TimeSpanType.NonZero);
+            TestPlaceholders(translation, MessageKey.TimeSpanType.Positive);
+            TestPlaceholders(translation, MessageKey.TimeSpanType.NonPositive);
+            TestPlaceholders(translation, MessageKey.TimeSpanType.Negative);
+            TestPlaceholders(translation, MessageKey.TimeSpanType.NonNegative);
+        }
+
+        private static void TestPlaceholders(IReadOnlyDictionary<string, string> translation, string key, params string[] allowedRulePlaceholders)
+        {
+            if (!translation.ContainsKey(key))
+            {
+                return;
+            }
+
+            var message = translation[key];
+
+            var placeholders = ArgHelper.ExtractPlaceholders(message);
+
+            var globalPlaceholders = new[]
+            {
+                "_name", "_translation"
+            };
+
+            foreach (var placehodler in placeholders)
+            {
+                var placeholderIsAllowed = allowedRulePlaceholders.Concat(globalPlaceholders).Any(p => string.Equals(placehodler.Name, p, StringComparison.Ordinal));
+
+                placeholderIsAllowed.Should().BeTrue($"Placeholder `{placehodler.Name}` is not allowed in message `{message}`");
             }
         }
     }
